a67c0110a0b9e2107f665bea15ba28e0354f169f
Andrew <moore.andrew09@gmail.com>
2015-04-28 20:43:12 -0400

    processing_script added, cleaned up helper functions

--- base_script.R
+++ base_script.R
@@ -1,27 +1,28 @@
 library(httr) 
 library(dplyr)
 library(jsonlite)
-library(lubridate)
+
+source("APIQueries.R")
 
 ## Sys.sleep(seconds)
 ## This can throttle the loop in case of overquerying
+
+script <- function(list, outputpath) {
+    for(user in mylist) {
+        
+        playerid <- getplayerid(user, keyfile)
+        
+        playerhist <- getmatchhist(playerid, keyfile)
+        
+        filename <- paste(outputpath, user, Sys.Date(), ".json", sep = "")
+        
+        write(playerhist, filename)
+    }
+}
 
 keyfile <- readLines("keyfile.txt", n = 1)
 
 mylist <- c("highronic", "suntso")
 path <- "./json-files/"
-
-script <- function(list, outputpath) {
-    for(user in mylist) {
-        
-       playerid <- getplayerid(user, keyfile)
-       
-       playerhist <- getmatchhist(playerid, keyfile)
-       
-       filename <- paste(outputpath, user, Sys.Date(), ".json", sep = "")
-       
-       write(playerhist, filename)
-    }
-}
 
 script(mylist, path)
