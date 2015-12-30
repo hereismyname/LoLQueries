b022780a09a6dea0fe09310cd6a7ccde60186908
Andrew Moore <hereismyname@users.noreply.github.com>
2015-12-30 16:15:02 -0700

    Update base_script.R
    - add some comments/documentation

--- base_script.R
+++ base_script.R
@@ -7,6 +7,13 @@ source("APIQueries.R")
 ## Sys.sleep(seconds)
 ## This can throttle the loop in case of overquerying
 
+# Pull down individual player stats for a given user (or set of users)
+#
+# Args:
+#   list: list of summoner names
+#   outputpath: where the player stats should be written out
+#
+# Returns: Nothing, files are written out. Files will be saved in JSON format.
 script <- function(list, outputpath) {
     for(user in mylist) {
         
@@ -18,8 +25,10 @@ script <- function(list, outputpath) {
         
         write(playerhist, filename)
     }
+    NULL
 }
 
+# this object should be a single character vector containing *your* personal API key
 keyfile <- readLines("keyfile.txt", n = 1)
 
 mylist <- c("highronic", "suntso")
